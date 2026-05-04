1ccd1f3ba1e21d1abf4852f62ae944a79affbfd8
rasmus <mail@rasmusantons.de>
2026-05-04 21:37:17 +0200

    fix CheckpointPositionStorage

--- src/main/java/de/rasmusantons/CheckpointPositionStorage.java
+++ src/main/java/de/rasmusantons/CheckpointPositionStorage.java
@@ -1,17 +1,17 @@
 package de.rasmusantons;
 
 import com.mojang.serialization.Codec;
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.world.level.saveddata.SavedDataType;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CheckpointPositionStorage extends SavedData {
-    public static final Codec<CheckpointPositionStorage> CODEC  = Codec.unboundedMap(Codec.INT, BlockPos.CODEC)
+    public static final Codec<CheckpointPositionStorage> CODEC  = Codec.unboundedMap(Codec.STRING, BlockPos.CODEC)
             .fieldOf("checkpoint_positions")
             .codec()
             .xmap(CheckpointPositionStorage::new, CheckpointPositionStorage::getCheckpointPositions);
@@ -23,29 +23,29 @@ public class CheckpointPositionStorage extends SavedData {
             null
     );
 
-    private final Map<Integer, BlockPos> checkpointPositions = new Int2ObjectOpenHashMap<>();
+    private final Map<String, BlockPos> checkpointPositions = new HashMap<>();
 
     private CheckpointPositionStorage() {
     }
 
-    private CheckpointPositionStorage(Map<Integer, BlockPos> checkpointPositions) {
-
+    private CheckpointPositionStorage(Map<String, BlockPos> checkpointPositions) {
+        this.checkpointPositions.putAll(checkpointPositions);
     }
 
-    private Map<Integer, BlockPos> getCheckpointPositions() {
+    private Map<String, BlockPos> getCheckpointPositions() {
         return checkpointPositions;
     }
 
     public void set(int checkpointNumber, BlockPos pos) {
-        BlockPos oldValue = checkpointPositions.get(checkpointNumber);
+        BlockPos oldValue = checkpointPositions.get(String.valueOf(checkpointNumber));
         if (oldValue == null || !oldValue.equals(pos)) {
-            checkpointPositions.put(checkpointNumber, pos);
+            checkpointPositions.put(String.valueOf(checkpointNumber), pos);
             setDirty();
         }
     }
 
     public BlockPos get(int checkpointNumber) {
-        return checkpointPositions.get(checkpointNumber);
+        return checkpointPositions.get(String.valueOf(checkpointNumber));
     }
 
     public static CheckpointPositionStorage getStorage(ServerLevel level) {

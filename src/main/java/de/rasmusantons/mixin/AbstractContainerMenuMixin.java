cfb9e11a414481165473a9f2fbb2a38c543ae4cc
rasmus <mail@rasmusantons.de>
2026-05-07 16:37:16 +0200

    only check for container clicks in lobby

--- src/main/java/de/rasmusantons/mixin/AbstractContainerMenuMixin.java
+++ src/main/java/de/rasmusantons/mixin/AbstractContainerMenuMixin.java
@@ -1,8 +1,10 @@
 package de.rasmusantons.mixin;
 
+import de.rasmusantons.BingoLobby;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
@@ -33,36 +35,38 @@ public class AbstractContainerMenuMixin {
 
     @Inject(method = "doClick", at = @At(value = "HEAD"), cancellable = true)
     protected void createMenu(int slotIndex, int buttonNum, ContainerInput containerInput, Player player, CallbackInfo ci) {
-        try {
-            if (player.gameMode() == GameType.CREATIVE) {
-                return;
-            }
-            Slot slot = slots.get(slotIndex);
-            CustomData customData = slot.getItem().getComponents().get(DataComponents.CUSTOM_DATA);
-            if (customData == null) {
-                return;
-            }
-            Optional<String> optionalBingoTag = customData.copyTag().getString("bingo_tag_selection");
-            if (optionalBingoTag.isEmpty()) {
-                return;
-            }
-            String bingoTag = optionalBingoTag.get();
-            ItemStack newItem;
-            var storage = Objects.requireNonNull(player.level().getServer()).getCommandStorage().get(Identifier.fromNamespaceAndPath("bingo", "vars"));
-            if (storage.getString("tag_" + bingoTag).orElse(DISABLED_TAG).equals(DISABLED_TAG)) {
-                storage.putString("tag_" + bingoTag, "enigmaticsbingogoals:" + bingoTag);
-                newItem = new ItemStack(Items.CARVED_PUMPKIN);
+        if (player instanceof ServerPlayer serverPlayer && serverPlayer.level().getGameRules().get(BingoLobby.BINGO_LOBBY)) {
+            try {
+                if (player.gameMode() == GameType.CREATIVE) {
+                    return;
+                }
+                Slot slot = slots.get(slotIndex);
+                CustomData customData = slot.getItem().getComponents().get(DataComponents.CUSTOM_DATA);
+                if (customData == null) {
+                    return;
+                }
+                Optional<String> optionalBingoTag = customData.copyTag().getString("bingo_tag_selection");
+                if (optionalBingoTag.isEmpty()) {
+                    return;
+                }
+                String bingoTag = optionalBingoTag.get();
+                ItemStack newItem;
+                var storage = Objects.requireNonNull(player.level().getServer()).getCommandStorage().get(Identifier.fromNamespaceAndPath("bingo", "vars"));
+                if (storage.getString("tag_" + bingoTag).orElse(DISABLED_TAG).equals(DISABLED_TAG)) {
+                    storage.putString("tag_" + bingoTag, "enigmaticsbingogoals:" + bingoTag);
+                    newItem = new ItemStack(Items.CARVED_PUMPKIN);
 
-            } else {
-                storage.putString("tag_" + bingoTag, DISABLED_TAG);
-                newItem = new ItemStack(Items.JACK_O_LANTERN);
+                } else {
+                    storage.putString("tag_" + bingoTag, DISABLED_TAG);
+                    newItem = new ItemStack(Items.JACK_O_LANTERN);
+                }
+                newItem.copyFrom(DataComponents.CUSTOM_DATA, slot.getItem());
+                newItem.copyFrom(DataComponents.ITEM_NAME, slot.getItem());
+                slot.set(newItem);
+                player.level().playSound(null, player, SoundEvents.LEVER_CLICK, SoundSource.BLOCKS, 1f, 1f);
+                ci.cancel();
+            } catch (Exception _) {
             }
-            newItem.copyFrom(DataComponents.CUSTOM_DATA, slot.getItem());
-            newItem.copyFrom(DataComponents.ITEM_NAME, slot.getItem());
-            slot.set(newItem);
-            player.level().playSound(null, player, SoundEvents.LEVER_CLICK, SoundSource.BLOCKS, 1f, 1f);
-            ci.cancel();
-        } catch (Exception _) {
         }
     }
 }
